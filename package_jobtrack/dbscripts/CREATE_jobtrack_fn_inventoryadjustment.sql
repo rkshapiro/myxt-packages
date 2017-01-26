dd04ecbc1bd983853edf847b02c9de32ab74f4bd
rkshapiro <rshapiro@xtuple.com>
2017-01-26 17:08:30 -0500

    inital commit
    jobtrack integration for insignia

---
+++ package_jobtrack/dbscripts/CREATE_jobtrack_fn_inventoryadjustment.sql
@@ -0,0 +1,90 @@
+-- Function: _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer)
+
+-- DROP FUNCTION _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer, integer);
+
+CREATE OR REPLACE FUNCTION _jobtrack.inventoryadjustment(pitemnumber character varying, pwarehouscode character varying, ptrxtype character varying, pqty numeric, pordernumber character varying, pjobnumber character varying, puser character varying, pinvhistid integer, pitemlocseries integer DEFAULT NULL::integer)
+  RETURNS integer AS
+$BODY$
+DECLARE
+_itemlocSeries		INTEGER;
+_r			RECORD;
+_invhistidNew		INTEGER;
+_result			BOOLEAN;
+_comment		text;
+
+BEGIN 
+
+-- 20170117 inital version for _jobtrack integration
+-- ptrxtype	values [+AD | SI | CC | -AD | -AD]
+-- pordernumber	is order number-line number
+-- TBD add location specifics
+              
+-- get the itemsite_id
+SELECT itemsite_id,costcat_asset_accnt_id,costcat_liability_accnt_id,item_number,item_id INTO _r
+FROM itemsite
+JOIN item ON itemsite_item_id = item_id
+JOIN whsinfo ON itemsite_warehous_id = warehous_id
+JOIN costcat ON itemsite_costcat_id = costcat_id
+WHERE item_number = pitemnumber
+AND warehous_code = pwarehouscode;
+
+-- Determine the item location series.
+IF (pitemlocseries IS NOT NULL) THEN
+	_itemlocSeries = pitemlocseries;	
+ELSE 
+	-- We are going make a determination on what to do based on pinvhistid.
+	IF (pinvhistid IS NOT NULL) THEN
+		SELECT invhist_series INTO _itemlocSeries
+		FROM invhist
+		WHERE invhist_id=pinvhistid;
+	ELSE
+		SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
+	END IF;
+END IF;
+
+-- construct the comment that will be used in postinvtrans
+IF (ptrxtype='SI') THEN
+	IF (pqty<0) THEN
+		_comment = 'JobTrack Reverse Material Scrap';
+	ELSE
+		_comment = 'JobTrack Material Scrap';
+	END IF;
+ELSIF (ptrxtype='CC') THEN
+        _comment = 'JobTrack Cycle Count';
+ELSE
+	_comment = 'JobTrack Adjustment';
+END IF;
+
+IF (ptrxtype='CC') THEN
+	_comment = _comment || ' for item '||_r.item_number||' by '||puser||' at location '||pjobnumber; 
+ELSE
+	_comment = _comment || ' for item '||_r.item_number||' by '||puser||' on Job#'||pjobnumber;
+END IF;
+
+
+-- post the inventory transaction that will always be an Adjustment to Inventory Material
+SELECT postinvtrans(
+_r.itemsite_id,
+ptrxtype::text, -- AD, CC, SI
+pqty,
+'I/M'::text,
+ptrxtype::text,		-- OrderType 
+pordernumber::text, -- order number
+pordernumber::text, -- doc number
+_comment::text,
+_r.costcat_asset_accnt_id,
+_r.costcat_liability_accnt_id,
+_itemlocSeries) INTO _invhistidNew;	
+
+SELECT postitemlocseries(_itemlocSeries) INTO _result; -- calls postinvhist that updates itemsite_qtyonhand
+
+RETURN _invhistidNew;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer, integer) OWNER TO "admin";
+GRANT EXECUTE ON FUNCTION _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer, integer) TO public;
+GRANT EXECUTE ON FUNCTION _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer, integer) TO "admin";
+GRANT EXECUTE ON FUNCTION _jobtrack.inventoryadjustment(character varying, character varying, character varying, numeric, character varying, character varying, character varying, integer, integer) TO xtrole;
---
+++ package_jobtrack/dbscripts/CREATE_jobtrack_vw_xt_item.sql
@@ -0,0 +1,57 @@
+-- View: _jobtrack.xt_item
+
+-- DROP VIEW _jobtrack.xt_item;
+
+CREATE OR REPLACE VIEW _jobtrack.xt_item AS 
+ SELECT 
+	item.item_number::character varying(255) AS item_number, 
+	item.item_active AS active, 
+	item.item_descrip1::character varying(255) AS description1, 
+	item.item_descrip2::character varying(255) AS description2, 
+        CASE
+            WHEN item.item_type = 'P'::bpchar THEN 'Purchased'::text
+            WHEN item.item_type = 'M'::bpchar THEN 'Manufactured'::text
+            WHEN item.item_type = 'J'::bpchar THEN 'Job'::text
+            WHEN item.item_type = 'K'::bpchar THEN 'Kit'::text
+            WHEN item.item_type = 'F'::bpchar THEN 'Phantom'::text
+            WHEN item.item_type = 'R'::bpchar THEN 'Reference'::text
+            WHEN item.item_type = 'S'::bpchar THEN 'Costing'::text
+            WHEN item.item_type = 'T'::bpchar THEN 'Tooling'::text
+            WHEN item.item_type = 'O'::bpchar THEN 'Outside Process'::text
+            WHEN item.item_type = 'L'::bpchar THEN 'Planning'::text
+            WHEN item.item_type = 'B'::bpchar THEN 'Breeder'::text
+            WHEN item.item_type = 'C'::bpchar THEN 'Co-Product'::text
+            WHEN item.item_type = 'Y'::bpchar THEN 'By-Product'::text
+            ELSE NULL::text
+        END::character varying(255) AS item_type, 
+        item.item_maxcost AS maximum_desired_cost, 
+        classcode.classcode_code::character varying(255) AS class_code, 
+        i.uom_name::character varying(255) AS inventory_uom, 
+        item.item_picklist AS pick_list_item, 
+        item.item_fractional AS fractional, 
+        item.item_config AS configured, 
+        item.item_sold AS item_is_sold, 
+        prodcat.prodcat_code::character varying(255) AS product_category, 
+        item.item_exclusive AS exclusive, 
+        item.item_listprice AS list_price, 
+        p.uom_name::character varying(255) AS list_price_uom, 
+        item.item_upccode::character varying(255) AS upc_code, 
+        item.item_prodweight AS product_weight, 
+        item.item_packweight AS packaging_weight, 
+        item.item_comments::character varying(255) AS notes, 
+        item.item_extdescrip::character varying(255) AS ext_description,
+        itemsite.itemsite_qtyonhand AS qtyonhand,
+        (SELECT warehous_code from whsinfo where warehous_id = itemsite_warehous_id)::character varying(255) AS site
+   FROM item
+   JOIN itemsite ON itemsite_item_id = item_id
+   JOIN classcode ON item.item_classcode_id = classcode.classcode_id
+   JOIN uom i ON item.item_inv_uom_id = i.uom_id
+   JOIN uom p ON item.item_price_uom_id = p.uom_id
+   LEFT JOIN prodcat ON item.item_prodcat_id = prodcat.prodcat_id
+  ORDER BY item.item_number::character varying(255);
+
+ALTER TABLE _jobtrack.xt_item
+  OWNER TO admin;
+GRANT ALL ON TABLE _jobtrack.xt_item TO admin;
+GRANT SELECT ON TABLE _jobtrack.xt_item TO public;
+GRANT SELECT, UPDATE, INSERT, REFERENCES, TRIGGER ON TABLE _jobtrack.xt_item TO xtrole;
