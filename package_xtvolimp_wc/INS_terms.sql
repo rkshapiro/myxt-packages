9a44c8032790afb3113866af9067f8fa8df9daf4
rkshapiro <rshapiro@xtuple.com>
2017-04-26 10:24:44 -0400

    returns change

---
+++ package_xtvolimp_wc/INS_terms.sql
@@ -0,0 +1,18 @@
+INSERT INTO terms(
+            terms_code, terms_descrip, terms_type, terms_duedays, 
+            terms_discdays, terms_discprcnt, terms_cutoffday, terms_ap, terms_ar, 
+            terms_fincharg)
+    VALUES ('VISA', 'Visa', 'D', 0, 
+            0, 0.00, 0, false, true, false);
+INSERT INTO terms(
+            terms_code, terms_descrip, terms_type, terms_duedays, 
+            terms_discdays, terms_discprcnt, terms_cutoffday, terms_ap, terms_ar, 
+            terms_fincharg)
+    VALUES ('MC', 'Master Card', 'D', 0, 
+            0, 0.00, 0, false, true, false);
+INSERT INTO terms(
+            terms_code, terms_descrip, terms_type, terms_duedays, 
+            terms_discdays, terms_discprcnt, terms_cutoffday, terms_ap, terms_ar, 
+            terms_fincharg)
+    VALUES ('AMAZON', 'Amazon', 'D', 0, 
+            0, 0.00, 0, false, true, false);
