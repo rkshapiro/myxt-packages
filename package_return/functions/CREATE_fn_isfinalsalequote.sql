717907f519c8ab79ab5e0d525d536c01f572bcd1
rkshapiro <rshapiro@xtuple.com>
2017-07-05 11:56:52 -0400

    moved isfinalsalequote to return package

---
+++ package_return/functions/CREATE_fn_isfinalsalequote.sql
@@ -0,0 +1,26 @@
+﻿-- Function: isfinalsalequote(integer)
+
+-- DROP FUNCTION isfinalsalequote(integer);
+
+CREATE OR REPLACE FUNCTION isfinalsalequote(integer)
+  RETURNS boolean AS
+$BODY$
+DECLARE
+  pquheadid ALIAS FOR $1;
+BEGIN
+
+PERFORM * FROM quhead
+WHERE (UPPER(quhead_shipcomments) ~ 'SALE' OR UPPER(quhead_custponumber) ~ 'SALE')
+AND quhead_id = pquheadid;
+
+IF (FOUND) THEN
+	RETURN TRUE;
+ELSE
+	RETURN FALSE;
+END IF;
+
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+ALTER FUNCTION isfinalsalequote(integer) OWNER TO "admin";
